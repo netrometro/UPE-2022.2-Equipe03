d1a67ceba6a2dc041a37fe174cb2d631b172f518
Pedro <97134972+mightbehappyy@users.noreply.github.com>
2023-03-04 23:28:50 -0300

    Nomes
    Mudei o nome dos arquivos

--- site/src/App.js
+++ site/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import React from 'react';
 import  {Login}  from './pages/Authentication/Login'
 import {Routes, Route} from 'react-router-dom';
-import { Loja } from './pages/loja/Loja';
+import Store from './pages/Store/Store';
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Login/>}></Route>
-      <Route path='loja' element={<Loja/>}></Route>
+      <Route path='store' element={<Store/>}></Route>
+      
     
     </Routes>
   );
---
+++ site/src/pages/Store/Store.js
@@ -0,0 +1,10 @@
+import './Style.css';
+
+
+export function Store(){
+    return(
+        <div>Ola</div>
+    )
+}
+
+export default Store;
