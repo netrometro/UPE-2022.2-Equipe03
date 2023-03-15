a16b5193dde49b145ee3a6e207416579e1c708eb
Pedro <97134972+mightbehappyy@users.noreply.github.com>
2023-03-15 13:42:19 -0300

    Aparência

--- site/src/pages/Components/CardGatex.jsx
+++ site/src/pages/Components/CardGatex.jsx
@@ -2,27 +2,34 @@ import React from "react";
 
 const CardGatex = ({image, name, type, desc}) => {
   let textColor;
+  let bgColor;
   switch (type) {
     case "Legendary":
-      textColor = "purple";
+      textColor = "white";
+      bgColor= "purple";
       break;
     case "Epic":
-      textColor = "blue";
+      textColor = "green";
+      bgColor = "orange";
       break;
     case "Rare":
-      textColor = "green";
+      textColor = "#ffff64";
+      bgColor = "#64b2ff";
       break;
     case "Common":
       textColor = "#876852";
+      bgColor = "#CBD6C8";
       break;
     default:
       textColor = "black";
+      bgColor = "#CBD6C8";
   }
 
   const typeStyle = { color: textColor };
+  const cardStyle = { backgroundColor: bgColor };
 
   return (
-    <div className="cardGatex" data-description={desc}>
+    <div className="cardGatex" data-description={desc} style={cardStyle}>
       <img src={image} alt={name} />
       <h3>{name}</h3>
       <h4 style={typeStyle}>{type}</h4>
