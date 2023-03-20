a9e65087e865ea17ebe5ae667f569003355ea80c
Pedro <97134972+mightbehappyy@users.noreply.github.com>
2023-03-20 13:42:37 -0300

    Grid
    Mudei o grid para as cartas novas aparecerem no topo

--- site/src/pages/Components/CardGridInv.jsx
+++ site/src/pages/Components/CardGridInv.jsx
@@ -14,7 +14,7 @@ const CardGridInv = () => {
     Axios
       .get(`http://localhost:3030/inventario/${invId}`)
       .then((response) => {
-        setCards(response.data);
+        setCards(response.data.reverse());
         console.log(response.data)
       })
       .catch((error) => {
@@ -24,7 +24,7 @@ const CardGridInv = () => {
     Axios
       .get(`http://localhost:3030/inventario/Pac/${invId}`)
       .then((response) => {
-        setStickerPack(response.data);
+        setStickerPack(response.data.reverse());
         console.log(response.data)
       })
       .catch((error) => {
@@ -34,12 +34,12 @@ const CardGridInv = () => {
 
   return (
     <div className="card-grid">
+      {stickerPack && stickerPack.map((pack) => (
+      <StickerPackInv key={pack.pacprodId} pacprodId={pack.pacprodId} image={pack.image} name={pack.name} price={pack.price}/>
+    ))}
       {Array.isArray(cards) && cards.map((card) => (
         <Card gatId={card.gatId} image={card.image} name={card.name} price={card.price} />
       ))}
-       {stickerPack && stickerPack.map((pack) => (
-      <StickerPackInv key={pack.pacprodId} pacprodId={pack.pacprodId} image={pack.image} name={pack.name} price={pack.price}/>
-    ))}
     </div>
   );
 };
